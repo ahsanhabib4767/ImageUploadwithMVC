0141e4a071ce131afc62c74788133c6b7ddc8c0d
Syed Ahsan Habib <37879412+ahsanhabib4767@users.noreply.github.com>
2019-03-31 16:46:57 +0600

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ImageUpload.Models;
+
+namespace ImageUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            TestEntities db = new TestEntities();
+            return View(db.tblFiles.ToList());
+        }
+        public ActionResult Add(HttpPostedFileBase postedFile)
+        {
+            byte[] bytes;
+            using (BinaryReader br = new BinaryReader(postedFile.InputStream))
+            {
+                bytes = br.ReadBytes(postedFile.ContentLength);
+            }
+            TestEntities db = new TestEntities();
+            db.tblFiles.Add(new tblFile {
+                Name = Path.GetFileName(postedFile.FileName),
+                ContentType = postedFile.ContentType,
+                Data = bytes
+
+            });
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+               
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
