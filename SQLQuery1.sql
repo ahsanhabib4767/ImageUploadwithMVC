9cacaee912f5dcdfb259c123d946e47f99a9469a
Syed Ahsan Habib <37879412+ahsanhabib4767@users.noreply.github.com>
2019-03-31 16:49:28 +0600

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+use [MBKTest]
+CREATE TABLE [dbo].[tblFiles](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[ContentType] [nvarchar](200) NOT NULL,
+	[Data] [varbinary](max) NOT NULL,
+ CONSTRAINT [PK_tblFiles] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
